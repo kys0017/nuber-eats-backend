c2a0cf0c4ca57cf9be5a86570576ab168a0b56ad
kangyesong <kys0017@gmail.com>
2023-12-22 21:00:19 +0900

    typeORM 추가 및 설정, cross-env 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,28 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234', // host: 'localhost' 이면, password 를 물어보지 않음. password 가 달라도 pass!
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
