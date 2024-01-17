f5f5f2585608de5ca8e6713874d6c071162ed4a0
kangyesong <kys0017@gmail.com>
2024-01-18 01:33:23 +0900

    editUpdate 시 save(user) 로 entity 업데이 하도록 변경

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -74,7 +74,17 @@ export class UsersService {
     return this.users.findOne({ where: { id } });
   }
 
-  async editProfile(userId: number, editProfileInput: EditProfileInput) {
-    return this.users.update(userId, { ...editProfileInput });
+  async editProfile(
+    userId: number,
+    { email, password }: EditProfileInput,
+  ): Promise<User> {
+    const user = await this.users.findOne({ where: { id: userId } });
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    return this.users.save(user);
   }
 }
