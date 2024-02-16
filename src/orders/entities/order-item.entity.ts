965e185f9200f3b9dee959ce9143f741cd2e0577
kangyesong <kys0017@gmail.com>
2024-02-17 01:00:45 +0900

    👔 Order Items - entity 추가

---
+++ src/orders/entities/order-item.entity.ts
@@ -0,0 +1,21 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Dish, DishOption } from 'src/restaurants/entities/dish.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+
+@InputType('OrderItemInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class OrderItem extends CoreEntity {
+  // Dish 에선 OrderItem 을 어떻게 가져올 지 신경쓰지 않아도 된다.
+  // (e.g. dish => dish.orderItem 같은 것들)
+  // 단지 OrderItem 에서 dish 에 접근하기만 원함.
+  // 반대의 관계에서 어떻게 되는지 명시해 줄 필요없음. 항상 필요하지는 않음.
+  @Field((type) => Dish)
+  @ManyToOne((type) => Dish, { nullable: true, onDelete: 'CASCADE' })
+  dish: Dish;
+
+  @Field((type) => [DishOption], { nullable: true })
+  @Column({ type: 'json', nullable: true }) // dish option 은 entity 에 넣지 않겠다.
+  options?: DishOption[];
+}
--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -5,11 +5,12 @@ import {
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
+import { IsEnum, IsNumber } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import { OrderItem } from './order-item.entity';
 
 export enum OrderStatus {
   Pending = 'Pending',
@@ -45,16 +46,18 @@ export class Order extends CoreEntity {
   })
   restaurant: Restaurant;
 
-  @Field((type) => [Dish])
-  @ManyToMany((type) => Dish)
+  @Field((type) => [OrderItem])
+  @ManyToMany((type) => OrderItem)
   @JoinTable() // 소유하고 있는 쪽의 relation 에 추가. order 에서 어떤 고객이 dish 를 주문했는지 알 수 있으므로 order 에 명시.
-  dishes: Dish[];
+  items: OrderItem[];
 
   @Column({ nullable: true })
   @Field((type) => Float, { nullable: true })
+  @IsNumber()
   total?: number;
 
   @Column({ type: 'enum', enum: OrderStatus })
   @Field((type) => OrderStatus)
+  @IsEnum(OrderStatus)
   status: OrderStatus;
 }
