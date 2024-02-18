93f244b92279184273d880418fdde525cc382417
kangyesong <kys0017@gmail.com>
2024-02-19 01:17:41 +0900

    👔 Create Order part Four - OrderItemOption.choice 타입 수정

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,9 +4,11 @@ import { Order } from './entities/order.entity';
 import { OrderService } from './orders.service';
 import { OrderResolver } from './orders.resolver';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Restaurant])],
+  imports: [TypeOrmModule.forFeature([Order, Restaurant, OrderItem, Dish])],
   providers: [OrderService, OrderResolver],
 })
 export class OrdersModule {}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,14 +5,20 @@ import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOuput } from './dtos/create-order.dto';
 import { User } from 'src/users/entities/user.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItems: Repository<OrderItem>,
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Dish)
+    private readonly dishes: Repository<Dish>,
   ) {}
 
   async createOrder(
@@ -30,14 +36,26 @@ export class OrderService {
           error: 'Restaurant not found',
         };
       }
+      items.forEach(async (item) => {
+        const dish = await this.dishes.findOne({ where: { id: item.dishId } });
+        if (!dish) {
+          // abort this whole thing
+        }
+        await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+      });
 
-      const order = await this.orders.save(
-        this.orders.create({
-          customer,
-          restaurant,
-        }),
-      );
-      console.log(order);
+      // const order = await this.orders.save(
+      //   this.orders.create({
+      //     customer,
+      //     restaurant,
+      //   }),
+      // );
+      // console.log(order);
     } catch (error) {
       return {
         ok: false,
