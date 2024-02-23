fff211ffc8fc7b91c2768f7757a1e7a8c3d51e1d
kangyesong <kys0017@gmail.com>
2024-02-24 01:21:47 +0900

    ✨ takeOrder - 배달원 등록 dto, resolver, service 작성 및 등록 후 publish

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -17,6 +17,7 @@ import {
   PUB_SUB,
 } from 'src/common/common.constants';
 import { PubSub } from 'graphql-subscriptions';
+import { TakeOrderInput, TakeOrderOutput } from './dtos/take-order.dto';
 
 @Injectable()
 export class OrderService {
@@ -265,6 +266,43 @@ export class OrderService {
       return {
         ok: false,
         error: 'Could not edit order.',
+      };
+    }
+  }
+
+  async takeOrder(
+    driver: User,
+    { id: orderId }: TakeOrderInput,
+  ): Promise<TakeOrderOutput> {
+    try {
+      const order = await this.orders.findOne({ where: { id: orderId } });
+      if (!order) {
+        return {
+          ok: false,
+          error: 'Order not found',
+        };
+      }
+      if (order.driver) {
+        return {
+          ok: false,
+          error: 'This order already has a driver',
+        };
+      }
+      await this.orders.save({
+        id: orderId,
+        driver,
+      });
+      await this.pubSub.publish(NEW_ORDER_UPDATE, {
+        orderUpdates: { ...order, driver },
+      });
+
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not update order.',
       };
     }
   }
