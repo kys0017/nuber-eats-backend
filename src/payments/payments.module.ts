686013fa352d796ed751ad5c272b8fab66fa281c
kangyesong <kys0017@gmail.com>
2024-02-25 00:27:37 +0900

    🍱 createPayment part One - resolver, service 셋팅 및 작성, dto 작성

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PaymentService } from './payments.service';
+import { PaymentResolver } from './payments.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Payment])],
+  providers: [PaymentService, PaymentResolver],
+})
 export class PaymentsModule {}
