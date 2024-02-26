f90590797fa72a64fbcb56c296a05297db623699
kangyesong <kys0017@gmail.com>
2024-02-26 14:54:51 +0900

    ✨ createPayment part Two - resolver, service 작성

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentService } from './payments.service';
 import { PaymentResolver } from './payments.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
   providers: [PaymentService, PaymentResolver],
 })
 export class PaymentsModule {}
