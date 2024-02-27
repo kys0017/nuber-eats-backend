fde1ded45c2908f5faa5e1d33b728c1a3e7b23de
kangyesong <kys0017@gmail.com>
2024-02-27 21:13:51 +0900

    🔧 apollo graphql 스튜디오 - cors 허용

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'https://studio.apollographql.com', // 허용할 출처
+    methods: ['GET'], // 허용할 HTTP 메서드
+    allowedHeaders: 'Content-Type, Accept', // 허용할 헤더
+  });
+  await app.listen(4000);
 }
 bootstrap();
