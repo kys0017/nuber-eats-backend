30197d31af7391c5d2a6d485f11614b78520757b
kangyesong <kys0017@gmail.com>
2024-03-15 02:40:40 +0900

    ✨ File Upload part Two - @aws-sdk/client-s3 설치, 파일업로드 코드 추가

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,3 +1,4 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import {
   Controller,
   Post,
@@ -6,11 +7,38 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
+const client = new S3Client({});
+
+const BUCKET_NAME = 'kimchinubereats20070017';
+
 @Controller('uploads')
 export class UploadsController {
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file) {
+    // console.log(file);
+
+    try {
+      // 버킷 생성 (버킷이 없으면 생성. 한번만 하면 됨. 버킷 이름은 unique 해야한다.)
+      // const createBucketCommand = new CreateBucketCommand({
+      //   Bucket: 'kimchinubereats20070017',
+      // });
+      // const response = await client.send(createBucketCommand);
+
+      const objectName = `${Date.now() + file.originalname}`;
+
+      const command = new PutObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: objectName,
+        Body: file.buffer,
+        ACL: 'public-read',
+      });
+      await client.send(command);
+
+      const url = `https://${BUCKET_NAME}.s3.ap-southeast-2.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (err) {
+      return null;
+    }
   }
 }
