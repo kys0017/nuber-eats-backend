c17cbd5631aecb5fbbab047a77bcdbeec291bc66
kangyesong <kys0017@gmail.com>
2024-03-16 00:01:50 +0900

    🎨 Cache Optimization part One - 음식점 생성 res. 에 restaurantId 필드 추가

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,12 +1,6 @@
-import {
-  Field,
-  InputType,
-  ObjectType,
-  OmitType,
-  PickType,
-} from '@nestjs/graphql';
-import { Restaurant } from '../entities/restaurant.entity';
+import { Field, InputType, Int, ObjectType, PickType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Restaurant } from '../entities/restaurant.entity';
 
 @InputType()
 export class CreateRestaurantInput extends PickType(Restaurant, [
@@ -19,4 +13,7 @@ export class CreateRestaurantInput extends PickType(Restaurant, [
 }
 
 @ObjectType()
-export class CreateRestaurantOutput extends CoreOutput {}
+export class CreateRestaurantOutput extends CoreOutput {
+  @Field((type) => Int)
+  restaurantId?: number;
+}
