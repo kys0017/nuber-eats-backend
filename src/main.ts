be7445b351ab28401255fae573ca698057f23d06
kangyesong <kys0017@gmail.com>
2024-03-28 20:25:58 +0900

    🔧 cors url 추가

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+    origin: [
+      'http://localhost:3000',
+      'https://studio.apollographql.com',
+      'https://nuber-eats-frontend-kys.netlify.app',
+    ],
     methods: ['GET', 'POST'],
     allowedHeaders: 'Content-Type, Accept, x-jwt',
     credentials: true,
