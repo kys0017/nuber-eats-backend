62845bcc8de953637939e20660a85f2936086ebe
kangyesong <kys0017@gmail.com>
2024-03-28 14:34:23 +0900

    🔧 backend 리스닝 포트 수정

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, x-jwt',
     credentials: true,
   });
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
